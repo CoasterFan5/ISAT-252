65249e79327f1b311b949598b36f1d90ccc373d7
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-08-24 16:58:36 -0400

    add testing

--- assignment 2/age.py
+++ assignment 2/age.py
@@ -5,5 +5,6 @@ def birth_year_to_age(birth_year):
     return current_year - birth_year
 
 # inputs and stuff
-in_year = int(input("Enter a year: "))
-print("Someone born in " + str(in_year) + " would be " + str(birth_year_to_age(in_year)))
+if __name__ == '__main__':
+    in_year = int(input("Enter a year: "))
+    print("Someone born in " + str(in_year) + " would be " + str(birth_year_to_age(in_year)))
