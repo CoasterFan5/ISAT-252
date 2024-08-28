e72a9ccf936b9ecab4a9155e70fc19f0eb1c3559
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-08-28 10:31:09 -0400

    add examples and rename program 2

---
+++ Week 1/examples.py
@@ -0,0 +1,11 @@
+# Examples from the week
+
+name = "Alice"
+x = 32
+
+"""
+Commas in print statements are super good
+You don't have to concatenate, and you dont have to cast
+"""
+print(name + " " + str(x))
+print(name, x)
