4d838f63421fbb311e6a72687f0a825e8aeb07f6
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-09-16 10:20:07 -0400

    update practice system

--- _Practice Resources/code_parser.py
+++
@@ -1,9 +0,0 @@
-# Problem 2:
-# return each letter of the word in input on a new line
-
-def main(input: str):
-    out = ""
-    for letter in input:
-        out += letter + "\n"
-    return out
-test(main)
